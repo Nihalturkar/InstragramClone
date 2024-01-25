8ccfa4224138779485eba12c5134b3bcbe820bba
Nihal Turkar <nihalturkar533@gmail.com>
2024-01-25 15:38:30 +0530

    Insta-clone

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,7 @@
 const mongoose  = require("mongoose");
 const user = require("./users");
 
-mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
+mongoose.connect("mongodb+srv://nihalturkar7489:nihal%40123@cluster.e2gq5rd.mongodb.net/?authMechanism=DEFAULT");
 const postSchema =mongoose.Schema({
     picture:String,
     user:{
@@ -11,7 +11,7 @@ const postSchema =mongoose.Schema({
     caption:String,
     date:{
         type:Date,
-        default:Date.now,
+        default:Date.toLocalString,
     },
     likes:[{
         type:mongoose.Schema.Types.ObjectId,
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose  = require("mongoose");
 const plm =  require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
+mongoose.connect("mongodb+srv://nihalturkar7489:nihal%40123@cluster.e2gq5rd.mongodb.net/?authMechanism=DEFAULT");
 
 const userSchma =mongoose.Schema({
   username:String,
